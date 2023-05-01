7442ed8fce2346a41066e6ef38a5a9cf482be14d
alamincoders <developeralamin03@gmail.com>
2023-05-01 15:51:32 +0600

    2nd video watched

---
+++ Module-2/src/type.ts
@@ -0,0 +1,13 @@
+const username: string = "AL AMIN";
+
+const roll: number = 18;
+
+const married: boolean = false;
+
+const wife: null = null;
+
+let fatherInLaw: undefined | string;
+
+fatherInLaw = "Abul Hashem";
+
+console.log(username, roll, married, wife, fatherInLaw);
