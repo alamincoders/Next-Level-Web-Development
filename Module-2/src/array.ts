2095e1f0aafb9e617092f59b67887e3ee1bb48d7
alamincoders <developeralamin03@gmail.com>
2023-05-01 16:03:15 +0600

    3rd video watched

---
+++ Module-2/src/array.ts
@@ -0,0 +1,11 @@
+const students: string[] = ["Tamanna", "Barsha", "Rabeya"];
+
+const eidSalami: number[] = [4500, 100, 100];
+
+//  Array and Tuples in typescript
+
+const myLove: [string, boolean] = ["Father", true];
+
+// myLove[0] = 6526;  error
+
+myLove[0] = "Mother";
