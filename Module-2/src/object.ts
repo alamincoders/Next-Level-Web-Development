aede0b80ecc124a6a14cb312865ddc14d5584a32
alamincoders <developeralamin03@gmail.com>
2023-05-01 16:19:53 +0600

    4th video watched

---
+++ Module-2/src/object.ts
@@ -0,0 +1,24 @@
+const user: {
+  name: string;
+  username: string;
+  company: {
+    // name: "Programming Hero"; --- its a literal type
+    readonly name: string;
+    catchPhrase: number;
+    bs?: string;
+  };
+} = {
+  name: "Leanne Graham",
+  username: "Bret",
+  company: {
+    name: "Romaguera-Crona",
+    // catchPhrase: "Multi-layered client-server neural-net",  ---- 🙄 error
+    catchPhrase: 632,
+    // bs: "harness real-time e-markets",  -- no error- because it's a optional property
+  },
+};
+
+// ?  ---- optional syntax
+// user.company.name = "Nur Software";  ---  error : Cannot assign to 'name' because it is a read-only property.--
+
+// Learning --- object, literal types, optional types in typescript
