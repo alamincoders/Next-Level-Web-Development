1408519c867bda94423966186a2fc4674ce44c7f
alamincoders <developeralamin03@gmail.com>
2023-05-01 15:36:01 +0600

    first video watched

---
+++ Module-2/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const result = 5.6;
+const client = "Hosain";
+console.log(result);
+console.log(client);
---
+++ Module-2/src/index.ts
@@ -0,0 +1,6 @@
+const result: number = 5.6;
+
+const client: string = "Hosain";
+
+console.log(result);
+console.log(client);
