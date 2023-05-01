44b6a50eb9b00a110cce6d6b1458ec0498439c43
alamincoders <developeralamin03@gmail.com>
2023-05-01 17:58:44 +0600

    6th video watched

--- Module-2/src/functions.ts
+++ Module-2/src/functions.ts
@@ -31,3 +31,47 @@ const person: {
 // person.getBalance("5"); --- error message: Argument of type 'string' is not assignable to parameter of type 'number'. ----
 
 person.getBalance(5);
+
+// -----------------------------------
+
+// default parameters
+function addNormal2(x: number, y: number = 10): number {
+  // always last argument to set default parameters
+  return x + y;
+}
+
+addNormal2(5);
+
+// spread operator
+const myMaleFr: string[] = ["Ariful", "Sabbir"];
+const newFr: string[] = ["Nur", "Rofiqul"];
+
+myMaleFr.push(...newFr);
+// console.log(myMaleFr);
+
+// rest operators
+
+const greetFr = (...friends: string[]): void => {
+  friends.forEach((fr: string) => console.log(`Hi ${fr}, How are you?`));
+};
+
+// greetFr("Kashim", "Ariful", "Torikul", "Mahbub", "Sumaya");
+
+// Array and object destructuring operators
+
+const myArray: number[] = [5, 2, 5, 1, 3];
+//index:----0---------1---,...
+const [bestNumber, secondary] = myArray;
+// console.log(bestNumber, secondary);
+
+// object
+const myFrObj: {
+  nameFr: string;
+  country: string;
+} = {
+  nameFr: "Kashim",
+  country: "Bangladesh",
+};
+// ------------- name ellis's
+const { nameFr, country: city } = myFrObj;
+// console.log(nameFr, city);
