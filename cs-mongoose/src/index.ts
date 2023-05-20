b961137bef83eaa9a09be39c72fc19624d870132
alamincoders <developeralamin03@gmail.com>
2023-05-20 19:03:02 +0600

    mongoose connecting try

--- cs-mongoose/src/index.ts
+++ cs-mongoose/src/index.ts
@@ -1,1 +1,38 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
 
+// port
+const port: number = 5000;
+
+// app create
+const app: Application = express();
+
+// use cors
+app.use(cors());
+
+// json parser
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// database connection
+const dbConnect = async (): Promise<void> => {
+  console.log("Hello connection");
+};
+dbConnect();
+
+// get
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    status: "200",
+    message: "Successfully App Running",
+    data: { name: "Al Amin", age: 20 },
+  });
+});
+
+// app listen
+app.listen(port, () => console.log(`Server is running on port ${port}`));
