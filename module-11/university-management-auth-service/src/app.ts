b6238626fd81761bb0c94bf7ac27160213841341
alamincoders <developeralamin03@gmail.com>
2023-05-30 13:10:53 +0600

    setup dependency

---
+++ module-11/university-management-auth-service/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+// create app
+const app: Application = express();
+
+// use middleware
+app.use(cors());
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// create routes
+
+// default route
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Welcome to university management auth service backend`);
+});
+
+export default app;
