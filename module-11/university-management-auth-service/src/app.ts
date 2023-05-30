c2cc5bc04f7efb807e06dc55d2581809299d5039
alamincoders <developeralamin03@gmail.com>
2023-05-30 21:33:36 +0600

    eslinst and prettier setup

--- module-11/university-management-auth-service/src/app.ts
+++ module-11/university-management-auth-service/src/app.ts
@@ -1,21 +1,23 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+console.log('port')
 
 // create app
-const app: Application = express();
+const app: Application = express()
 
 // use middleware
-app.use(cors());
+app.use(cors())
 
 // body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // create routes
 
 // default route
-app.get("/", (req: Request, res: Response) => {
-  res.send(`Welcome to university management auth service backend`);
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Welcome to university management auth service backend`)
+})
 
-export default app;
+export default app
